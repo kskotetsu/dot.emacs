591ae684fa36f88c9a84b09ff31ca3f0b92c3655
kskotetsu <kskotetsu@gmail.com>
2013-10-30 13:00:07 +0900

    C++用設定他

--- inits/00-global.el
+++ inits/00-global.el
@@ -23,6 +23,8 @@
   (run-with-idle-timer 0.2 nil #'linum-update-current))
 
 (setq tab-width 4)
+(setq default-tab-width 4)
+(setq c-basic-offset 4)
 
 ;; マウスのホイールスクロールスピードを調節
 ;; (連続して回しているととんでもない早さになってしまう。特にLogicoolのマウス)
@@ -74,3 +76,61 @@
 (require 'ucs-normalize)
 (set-file-name-coding-system 'utf-8-hfs)
 (setq locale-coding-system 'utf-8)
+
+;; GCの頻度を増やす
+(setq gc-cons-threshold 100000) 
+;; GCの頻度を減らす
+;(setq gc-cons-threshold 5242880)
+
+;;---------------------------------------------------------------------
+;; 検索設定
+(setq case-fold-search t)               ;検索では大文字小文字を区別しない
+
+; フレームタイトルの設定
+(setq frame-title-format "%b")
+
+;; 重複ファイルを hoge<dir1>などのように
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+
+
+;; ------------------------------------------------------------------------
+;; C++モード
+; プリコンパイルヘッダの作り方
+; clang++ -cc1 -emit-pch -x c++-header ./stdafx.h -o stdafx.pch　-I(インクルードディレクトリ)
+(add-hook 'c++-mode-hook '(lambda ()
+			    ;(my-ac-cc-mode-setup)
+			    (gtags-mode 1)
+			    (setq c-auto-newline nil)
+			    
+			    (setq c-auto-newline nil)
+														;;(linum-mode)
+			    (setq c++-tab-always-indent nil)		; [TAB] キーで、TABコードを入力
+			    (setq c-tab-always-indent nil)			; [TAB] キーで、TABコードを入力
+			    (setq indent-tabs-mode t)
+			    (show-paren-mode t)						;対応する括弧を表示
+			    (setq tab-width 4)
+			    (c-toggle-hungry-state -1)
+			    (setq truncate-lines t)					;長い行を折り返し表示しない
+				;(setq show-paren-style 'expression)
+			    (c-set-style "stroustrup")	
+			    (local-set-key "\C-m" 'newline-and-indent)
+			    (local-set-key "\C-j" 'newline-and-indent)
+			    (setq dabbrev-case-fold-search nil)
+				;; (setq comment-start "// "
+				;; 		comment-end " "
+				;; 		)
+			    (font-lock-fontify-buffer)
+			    (setq font-lock-keywords c++-font-lock-keywords-2)
+				;; http://d.hatena.ne.jp/i_s/20091026/1256557730
+			    (c-set-offset 'innamespace 0)			; namespace {}の中はインデントしない
+			    (c-set-offset 'arglist-close 0)			; 関数の引数リストの閉じ括弧はインデントしない
+			    (c-set-offset 'label 0)
+			    (c-set-offset 'substatement-open 0)
+			    (c-set-offset 'statement-case-intro 2)
+			    (c-set-offset 'inline-open 0)
+			    (c-set-offset 'case-label 2)
+			    
+			    ))
--- inits/20-keybind.el
+++ inits/20-keybind.el
@@ -1,3 +1,23 @@
 ;
 (global-set-key '[f2] 'multi-term)
+;(global-set-key '[f9] 'ff-find-other-file)
+
+;; C-C bfpnでウィンドウ移動
+(global-set-key (kbd "C-c b") 'windmove-left)
+(global-set-key (kbd "C-c f") 'windmove-right)
+(global-set-key (kbd "C-c p") 'windmove-up)
+(global-set-key (kbd "C-c n") 'windmove-down)
+(global-set-key (kbd "C-c h") 'windmove-left)
+(global-set-key (kbd "C-c l") 'windmove-right)
+(global-set-key (kbd "C-c k") 'windmove-up)
+
+;(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(eval-after-load 'helm
+  '(progn
+     (define-key helm-map (kbd "C-h") 'delete-backward-char)
+     ))
+
+;; backspaceキーをインクリメンタルサーチ中のミニバッファで有効にする
+(define-key isearch-mode-map [backspace] 'isearch-delete-char)
+(define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
 
