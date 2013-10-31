f009ea202aaf02b90492efdcfa361cc0731cf43f
kskotetsu <kskotetsu@gmail.com>
2013-10-31 21:53:53 +0900

    画面関係の設定を移動

--- inits/00-global.el
+++ inits/00-global.el
@@ -5,59 +5,26 @@
 ;  ;; Terminal 上での動作設定
 ;  )
 
-;; スタート画面なしに
-;(setq inhibit-startup-message t)
-
-;; C-hはバックスペース
-(global-set-key "\C-h" 'delete-backward-char)
-
-;; scroll
-(setq scroll-conservatively 35
-       scroll-margin 3
-       scroll-step 1)
-
-(global-linum-mode t)
-(setq linum-delay t)
-;; 行番号高速化
-(defadvice linum-schedule (around my-linum-schedule () activate)
-  (run-with-idle-timer 0.2 nil #'linum-update-current))
-
-(setq tab-width 4)
-(setq default-tab-width 4)
-(setq c-basic-offset 4)
-
 ;; マウスのホイールスクロールスピードを調節
 ;; (連続して回しているととんでもない早さになってしまう。特にLogicoolのマウス)
-;; (defun scroll-down-with-lines ()
-;;   "" (interactive) (scroll-down 1))
-;; (defun scroll-up-with-lines ()
-;;   "" (interactive) (scroll-up 1))
-;; (global-set-key [wheel-up] 'scroll-down-with-lines)
-;; (global-set-key [wheel-down] 'scroll-up-with-lines)
-;; (global-set-key [double-wheel-up] 'scroll-down-with-lines)
-;; (global-set-key [double-wheel-down] 'scroll-up-with-lines)
-;; (global-set-key [triple-wheel-up] 'scroll-down-with-lines)
-;; (global-set-key [triple-wheel-down] 'scroll-up-with-lines)
+(defun scroll-down-with-lines ()
+  "" (interactive) (scroll-down 1))
+(defun scroll-up-with-lines ()
+  "" (interactive) (scroll-up 1))
+(global-set-key [wheel-up] 'scroll-down-with-lines)
+(global-set-key [wheel-down] 'scroll-up-with-lines)
+(global-set-key [double-wheel-up] 'scroll-down-with-lines)
+(global-set-key [double-wheel-down] 'scroll-up-with-lines)
+(global-set-key [triple-wheel-up] 'scroll-down-with-lines)
+(global-set-key [triple-wheel-down] 'scroll-up-with-lines)
 
 ;; ビープを鳴らさない
 (setq visible-bell nil)
-;; ツールバーをオフ
-(tool-bar-mode 0)
 
 ;; 最近開いたファイルをたくさん残す
 (recentf-mode)
 (setq recentf-max-menu-items 20)
 (setq recentf-max-saved-items 1000)
-
-;;起動時のフレームサイズを設定する
-(setq initial-frame-alist
-      (append (list
-        '(width . 120)
-        '(height . 48)
-        )
-        initial-frame-alist))
-(setq default-frame-alist initial-frame-alist)
-
 
 ;;--------------------------------------------------------------------;
 ;; 日本語設定
@@ -65,13 +32,11 @@
 ; 言語を日本語にする
 ;(set-language-environment 'Japanese)
 (set-language-environment 'utf-8)
-; 極力UTF-8とする
-(prefer-coding-system 'utf-8)
-;; クリップボードの文字コード
-(set-selection-coding-system 'utf-8)
-;; 端末の文字コード
-(set-terminal-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)			; 極力UTF-8とする
+(set-selection-coding-system 'utf-8)	; クリップボードの文字コード
+(set-terminal-coding-system 'utf-8)		; 端末の文字コード
 (set-keyboard-coding-system 'utf-8)
+
 ;; ファイル名の文字コード
 (require 'ucs-normalize)
 (set-file-name-coding-system 'utf-8-hfs)
@@ -79,6 +44,7 @@
 
 ;; GCの頻度を増やす
 (setq gc-cons-threshold 100000) 
+
 ;; GCの頻度を減らす
 ;(setq gc-cons-threshold 5242880)
 
@@ -86,15 +52,11 @@
 ;; 検索設定
 (setq case-fold-search t)               ;検索では大文字小文字を区別しない
 
-; フレームタイトルの設定
-(setq frame-title-format "%b")
-
-;; 重複ファイルを hoge<dir1>などのように
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-(setq uniquify-ignore-buffers-re "*[^*]+*")
-
+;IME ON/OFF時のカーソルカラー
+;(add-hook 'input-method-activate-hook
+;(lambda() (set-cursor-color "green")))
+;(add-hook 'input-method-inactivate-hook
+;(lambda() (set-cursor-color "white")))
 
 ;; ------------------------------------------------------------------------
 ;; C++モード
@@ -104,9 +66,7 @@
 			    ;(my-ac-cc-mode-setup)
 			    (gtags-mode 1)
 			    (setq c-auto-newline nil)
-			    
-			    (setq c-auto-newline nil)
-														;;(linum-mode)
+				;;(linum-mode)
 			    (setq c++-tab-always-indent nil)		; [TAB] キーで、TABコードを入力
 			    (setq c-tab-always-indent nil)			; [TAB] キーで、TABコードを入力
 			    (setq indent-tabs-mode t)
--- inits/10-display.el
+++ inits/10-display.el
@@ -1,4 +1,49 @@
-(el-get 'sync 'color-theme-solarized)
+;; スタート画面なしに
+;(setq inhibit-startup-message t)
 
+;; カラーテーマの設定
+(el-get 'sync 'color-theme-solarized)
 (load-theme 'solarized-dark t)
 ;(color-theme-sanityinc-solarized-dark)
+
+; 長い行を折り返し表示しない
+(setq truncate-lines t)
+;(setq truncate-lines nil)
+
+; ツールバーをオフ
+(tool-bar-mode 0)
+
+; フレームタイトルの設定
+(setq frame-title-format "%b")
+
+;;起動時のフレームサイズを設定する
+(setq initial-frame-alist
+      (append (list
+        '(width . 120)
+        '(height . 48)
+        )
+        initial-frame-alist))
+(setq default-frame-alist initial-frame-alist)
+
+;; 重複ファイルを hoge<dir1>などのように
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+
+
+(setq tab-width 4)
+(setq default-tab-width 4)
+(setq c-basic-offset 4)
+
+(global-linum-mode nil)
+(setq linum-delay t)
+
+;; 行番号高速化
+(defadvice linum-schedule (around my-linum-schedule () activate)
+  (run-with-idle-timer 0.2 nil #'linum-update-current))
+
+;; scroll
+(setq scroll-conservatively 35
+       scroll-margin 3
+       scroll-step 1)
