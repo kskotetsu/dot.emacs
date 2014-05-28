363fea9127e7692b485908d0447f303293d27f39
kskotetsu <kskotetsu@gmail.com>
2014-05-28 11:34:41 +0900

    windows用の文字コード設定

--- inits/windows-japanese.el
+++ inits/windows-japanese.el
@@ -1,1 +1,3 @@
-(set-file-name-coding-system 'cp932)
+﻿(set-file-name-coding-system 'cp932)
+(set-buffer-file-coding-system 'utf-8-with-signature-dos)
+;(global-set-key (kbd "C-o") 'toggle-input-method)
