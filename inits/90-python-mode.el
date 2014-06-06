d8f04e6c8024bd6bb1db29372888fd0ac93a12b0
kskotetsu <kskotetsu@gmail.com>
2014-06-06 15:44:16 +0900

    python-modeをel-getのものにする。ac-pythonは新しいemacsでは動かない模様

--- inits/90-python-mode.el
+++ inits/90-python-mode.el
@@ -1,4 +1,8 @@
 ;; Python
+;(el-get 'sync 'ac-python)
+(require 'python)
+;(require 'ac-python)
+
 (add-hook 'python-mode-hook
                    '(lambda()
                         (setq indent-tabs-mode nil)
@@ -7,6 +11,7 @@
                         (setq tab-width 4)
 						(flycheck-mode t)
 						))
+;(add-to-list 'ac-modes 'python-mode)
 
 ;(add-hook 'python-mode-hook 'flycheck-mode)
 
