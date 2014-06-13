5e81c6a2b206d9d8c68db4a64b17805a410d6b4c
kskotetsu <kskotetsu@gmail.com>
2014-06-13 12:22:05 +0900

    windows IME初期化

--- inits/windows-japanese.el
+++ inits/windows-japanese.el
@@ -1,3 +1,8 @@
 ﻿(set-file-name-coding-system 'cp932)
 (set-buffer-file-coding-system 'utf-8-with-signature-dos)
 ;(global-set-key (kbd "C-o") 'toggle-input-method)
+
+(setq default-input-method "W32-IME")
+(setq-default w32-ime-mode-line-state-indicator "[A]")
+(setq w32-ime-mode-line-state-indicator-list '("[A]" "[J]" "[A]"))
+(w32-ime-initialize)
