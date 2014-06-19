4085f4c4fcccea9a62f836df5dc6d3c30db4c018
kskotetsu <kskotetsu@gmail.com>
2014-06-19 16:19:20 +0900

    e2wmでレイアウト管理

--- init.el
+++ init.el
@@ -63,6 +63,7 @@
 	elscreen
 	;tabbar
 	gtags
+	e2wm
 	anzu
 	guide-key
 	;guide-key-tip
---
+++ inits/20-e2wm.el
@@ -0,0 +1,5 @@
+; -*- coding: utf-8 -*-
+
+(require 'e2wm)
+
+(global-set-key (kbd "M-+") 'e2wm:start-management)
