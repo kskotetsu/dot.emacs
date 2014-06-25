a003a7ba0c25baac8b105ba3d48297696c39284b
kskotetsu <kskotetsu@gmail.com>
2014-06-25 15:59:20 +0900

    rubyのflycheckをrubocopに

--- inits/90-ruby-mode.el
+++ inits/90-ruby-mode.el
@@ -1,7 +1,25 @@
 ;; Ruby
 (require 'enh-ruby-mode)
+(require 'flycheck)
 
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+;(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+
+(flycheck-define-checker ruby-rubocop
+  "A Ruby syntax and style checker using the RuboCop tool.
+
+See URL `http://batsov.com/rubocop/'."
+  :command ("rubocop" "--format" "emacs" "--silent"
+            (config-file "--config" flycheck-rubocoprc)
+            source)
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+            line-end)
+   (error line-start
+          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+          line-end))
+  :modes (enh-ruby-mode motion-mode))
+
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 ;; (setq auto-mode-alist
@@ -17,11 +35,16 @@
                         (setq tab-width 4)
 						(flycheck-mode t)
 						(ruby-electric-mode t)
-						(set-face-foreground 'enh-ruby-op-face "gray40")
-						(set-face-foreground 'enh-ruby-string-delimiter-face "gray40")
 						))
 
-(setq flycheck-rubocop-lint-only t)
+(eval-after-load "enh-ruby-mode"
+  '(progn
+	 (set-face-foreground 'enh-ruby-op-face "gray40")
+	 (set-face-foreground 'enh-ruby-string-delimiter-face "gray40")
+	 ))
+	 
+;(setq flycheck-rubocop-lint-only t)
+;(setenv GEM_PATH "C:/Ruby193/lib/ruby/gems/1.9.1")
 
 ;(autoload 'rubydb "rubydb3x" "run rubydb on program file in buffer" t)
 (autoload 'rubydb "rubydb3x"
