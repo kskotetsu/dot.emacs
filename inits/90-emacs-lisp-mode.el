3cb70be6804dcb291fe8d1d6a5897c77ae46213b
kskotetsu <kskotetsu@gmail.com>
2014-07-17 15:06:10 +0900

    emacs-lispでflycheckとeldoc

--- inits/90-emacs-lisp-mode.el
+++ inits/90-emacs-lisp-mode.el
@@ -1,5 +1,9 @@
 ;; emacs-lisp編集時設定
 
 ;; elispのときはflycheck
-(add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
+(add-hook 'emacs-lisp-mode-hook
+		  '(lambda()
+			 (flycheck-mode)
+			 (eldoc-mode)
+			 ))
 
