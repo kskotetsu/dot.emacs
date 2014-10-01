67e8fec59d6f65b9b001980a424aad7f439263bd
kskotetsu <kskotetsu@gmail.com>
2014-10-01 16:04:38 +0900

    elisp hookをlambdaから関数へ

--- inits/90-emacs-lisp-mode.el
+++ inits/90-emacs-lisp-mode.el
@@ -1,9 +1,11 @@
 ;; emacs-lisp編集時設定
 
+(defun my-elisp-mode ()
+  (flycheck-mode)
+  (eldoc-mode)
+  )
+
+
 ;; elispのときはflycheck
-(add-hook 'emacs-lisp-mode-hook
-		  '(lambda()
-			 (flycheck-mode)
-			 (eldoc-mode)
-			 ))
+(add-hook 'emacs-lisp-mode-hook 'my-elisp-mode)
 
