de320150fa91670b2fa774f3b97a476872ead84f
kskotetsu <kskotetsu@gmail.com>
2014-11-11 09:35:07 +0900

    cygwin設定

--- inits/windows-cygwin.el
+++ inits/windows-cygwin.el
@@ -26,9 +26,28 @@
 ;; ------------------------------------------------------------------------
 ;; @ shell
 (require 'shell)
-;; (setq explicit-shell-file-name "bash.exe")
-;; (setq shell-command-switch "-c")
-;; (setq shell-file-name "bash.exe")
+(setq explicit-shell-file-name "bash.exe")
+;(setq shell-command-switch "--noediting")
+(setq explicit-bash.exe-args '("--login" "--noediting" "-i"))
+(setq shell-file-name "bash.exe")
+
+;(setq explicit-shell-file-name "cmdproxy.exe")
+;(setq shell-file-name "cmdproxy.exe")
+
+;; http://d.hatena.ne.jp/armbrust/20120909/134718360
+;; fakecygcmdを使ってシェルのC-Cを効くようにする
+
+;; ssh の関数 の実装を流用して乗っ取る
+(defadvice comint-interrupt-subjob (around ad-comint-interrupt-subjob activate)
+  (process-send-string nil "\C-c"))
+
+;; shell 設定
+(setq explicit-shell-file-name "f_bash")
+(setq shell-file-name "bash") ; これは f_ にしてはいけない
+(setq shell-command-switch "-c")
+;(setq explicit-shell-file-name "f_cmd")
+;(setq shell-file-name "cmd") ; これは f_ にしてはいけない
+;(setq shell-command-switch "-c")
 
 ;; ;; (M-! and M-| and compile.el)
 ;; (setq shell-file-name "bash.exe")
@@ -48,3 +67,17 @@
 ;; ;  '(setq gnutls-trustfiles "c:/cygwin/usr/ssl/certs/ca-bundle.crt")
 
 (eval-after-load "gnutls" '(setq gnutls-trustfiles (mapcar 'expand-file-name gnutls-trustfiles)))
+
+;(executable-find "cmdproxy")
+(executable-find "bash")
+
+;; (defun my-shell-setup ()
+;;   "For Cygwin bash under Emacs 20"
+;;   (setq comint-scroll-show-maximum-output 'this)
+;;   (make-variable-buffer-local 'comint-completion-addsuffix))
+;; (setq comint-completion-addsuffix t)
+;; ;; (setq comint-process-echoes t) ;; reported that this is no longer needed
+;; (setq comint-eol-on-send t)
+;; (setq w32-quote-process-args ?\")
+
+;; (setq shell-mode-hook 'my-shell-setup)
