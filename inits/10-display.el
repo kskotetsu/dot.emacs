6e0bf2648ce19a02262c1b8c42751fa047997e44
kskotetsu <kskotetsu@gmail.com>
2014-11-13 09:20:41 +0900

    縦分割・横分割のトグル関数を追加

--- inits/10-display.el
+++ inits/10-display.el
@@ -95,3 +95,18 @@
 ;(require 'auto-highlight-symbol)
 ;(global-auto-highlight-symbol-mode t)
 
+(defun window-toggle-division ()
+  "ウィンドウ 2 分割時に、縦分割<->横分割"
+  (interactive)
+  (unless (= (count-windows 1) 2)
+    (error "ウィンドウが 2 分割されていません。"))
+  (let ((before-height)
+        (other-buf (window-buffer (next-window))))
+    (setq before-height (window-height))
+    (delete-other-windows)
+    (if (= (window-height) before-height)
+        (split-window-vertically)
+      (split-window-horizontally))
+    (other-window 1)
+    (switch-to-buffer other-buf)
+    (other-window -1)))
