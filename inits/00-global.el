a93deab4c99e5bc1a10445f6e67710115f80982d
kskotetsu <kskotetsu@gmail.com>
2014-11-14 12:52:48 +0900

    global-delimiter-modeはなくなったようなので削除

--- inits/00-global.el
+++ inits/00-global.el
@@ -141,9 +141,6 @@
 (cua-mode)
 (setq cua-enable-cua-keys nil)
 
-;; rainbow-delimiters-mode
-(global-rainbow-delimiters-mode)
-
 ;(add-to-list 'rainbow-delimiters-ignore-modes 'fundamental-mode) ;helmとの干渉回避
 ;(custom-set-faces '(rainbow-delimiters-depth-1-face
 ;					((t (:foreground "#586e75"))))) ;文字列の色と被るため,変更
