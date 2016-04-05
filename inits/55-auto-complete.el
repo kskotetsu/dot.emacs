8107e899ede9ac4e2beb4c2dad9be08dc7ed4713
kskotetsu <kskotetsu@gmail.com>
2016-04-05 16:12:08 +0900

    autocompleteからcompany-modeに乗り換えテスト

--- inits/55-auto-complete.el
+++ inits/55-auto-complete.el
@@ -1,13 +1,13 @@
 ;(el-get 'sync 'auto-complete)
 ;(el-get 'sync 'ac-dabbrev)
 
-(require 'auto-complete)
-(require 'ac-dabbrev)
+;; (require 'auto-complete)
+;; (require 'ac-dabbrev)
 
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(ac-set-trigger-key "TAB")
+;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+;; (ac-set-trigger-key "TAB")
 
-(require 'auto-complete-config)
+;; (require 'auto-complete-config)
 
-(ac-config-default)
-(global-auto-complete-mode t)
+;; (ac-config-default)
+;; (global-auto-complete-mode t)
---
+++ inits/55-company-mode.el
@@ -0,0 +1,48 @@
+; -*- coding: utf-8 -*-
+
+; http://qiita.com/sune2/items/b73037f9e85962f5afb7
+
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
+;; (define-key company-active-map (kbd "M-n") nil)
+;; (define-key company-active-map (kbd "M-p") nil)
+;; (define-key company-active-map (kbd "C-n") 'company-select-next)
+;; (define-key company-active-map (kbd "C-p") 'company-select-previous)
+;; (define-key company-active-map (kbd "C-h") nil)
+
+(defun company--insert-candidate2 (candidate)
+  (when (> (length candidate) 0)
+    (setq candidate (substring-no-properties candidate))
+    (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
+        (insert (company-strip-prefix candidate))
+      (if (equal company-prefix candidate)
+          (company-select-next)
+          (delete-region (- (point) (length company-prefix)) (point))
+        (insert candidate))
+      )))
+
+(defun company-complete-common2 ()
+  (interactive)
+  (when (company-manual-begin)
+    (if (and (not (cdr company-candidates))
+             (equal company-common (car company-candidates)))
+        (company-complete-selection)
+      (company--insert-candidate2 company-common))))
+
+(define-key company-active-map [tab] 'company-complete-common2)
+(define-key company-active-map [backtab] 'company-select-previous) ; おまけ
+
+(require 'color)
+
+;; 色設定
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
