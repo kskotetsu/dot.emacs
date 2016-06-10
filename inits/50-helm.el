edcea17dd30257e7af2a7539f2f2cae8298df1bc
kskotetsu <kskotetsu@gmail.com>
2016-06-10 14:49:33 +0900

    helm-agをprojectileを基本に

--- inits/50-helm.el
+++ inits/50-helm.el
@@ -95,7 +95,8 @@
 (setq helm-ag-base-command "ag --nocolor --nogroup -S")
 
 (global-set-key (kbd "C-M-o") 'helm-do-grep)
-(global-set-key (kbd "<f3>") 'helm-ag)
+;(global-set-key (kbd "<f3>") 'helm-ag)
+(global-set-key (kbd "<f3>") 'helm-ag-project-root)
 ;(global-set-key (kbd "C-M-o") 'helm-ag)
 (global-set-key (kbd "<f8>") 'helm-semantic-or-imenu)
 ;(global-set-key (kbd "<f8>") 'helm-imenu-anywhere)
